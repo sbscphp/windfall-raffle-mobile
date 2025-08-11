3f6bcaffdb36c8bce7c6ac84d1ddc811c826beb1
Daniel <danielisiogugu@heymoni.com>
2025-08-11 06:21:56 +0100

    added desugaring support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // Flag to enable support for the new language APIs,[flutter_local_notifications]
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -24,10 +26,11 @@ android {
         applicationId = "com.windfall.windfall"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23 // Minimum SDK version supported by the app,as required by firebase_messaging
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,4 +44,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // added for desugaring support[flutter_local_notifications]
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
