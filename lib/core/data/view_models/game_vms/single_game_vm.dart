3ec8fb7675a4489e29fee89d2b49e82191e20963
ayodejiogundijo <ayodeji@rank.africa>
2025-09-08 20:31:36 +0100

    game details integration

--- lib/core/data/view_models/game_vms/single_game_vm.dart
+++ lib/core/data/view_models/game_vms/single_game_vm.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:windfall/core/data/models/discount_tier.dart';
 import 'package:windfall/core/data/models/ticket_tier.dart';
